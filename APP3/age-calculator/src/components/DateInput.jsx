ce9fa266c985ab1bad9941550bbd276ead96e0f6
VISH01AL <g.vish1al@gmail.com>
2024-01-27 11:42:36 -0500

    added the error states

--- APP3/age-calculator/src/components/DateInput.jsx
+++ APP3/age-calculator/src/components/DateInput.jsx
@@ -6,26 +6,26 @@ export const DateInput = ({calculateage}) => {
   const [months,setMonths]=useState("")
   const [years,setYears]=useState("")
 
-  const [dayerror,setDayError]=useState("This field is required")
-  const [montherror,setMonthError]=useState("for months")
-  const [yearerror,setYearError]=useState("for years")
+  const [dayerror,setDayError]=useState({inerrorstate:false,value:"for days"})
+  const [montherror,setMonthError]=useState({inerrorstate:false,value:"for months"})
+  const [yearerror,setYearError]=useState({inerrorstate:false,value:"for years"})
   return (
     <div>
         <div className='input-div'>
           <div className='miniconatiner'>
-            <label>DAY</label>
-            <input type='text' onChange={(e)=>{setDays(e.target.value)}} value={days} placeholder='DD'/>
-            <div className='error' id="errordate">{dayerror}</div>
+            <label className={dayerror.inerrorstate ? 'datelabel errorlabel': 'datelabel errorfreelabel'}>DAY</label>
+            <input type='text' onChange={(e)=>{setDays(e.target.value)}} value={days} placeholder='DD' className={dayerror.inerrorstate ? 'dateinput errorinput': 'dateinput errorfreeinput'}/>
+            <div className={dayerror.inerrorstate ? 'error': 'errorfree'} >{dayerror.value}</div>
           </div>
           <div className='miniconatiner'>
-            <label>MONTH</label>
-            <input type='text' onChange={(e)=>{setMonths(e.target.value)}} value={months} placeholder='MM'/>
-            <div className='error' id="errormonth">{montherror}</div>
+            <label className={montherror.inerrorstate ? 'datelabel errorlabel': 'datelabel errorfreelabel'}>MONTH</label>
+            <input type='text' onChange={(e)=>{setMonths(e.target.value)}} value={months} placeholder='MM' className={montherror.inerrorstate ? 'dateinput errorinput': 'dateinput errorfreeinput'}/>
+            <div className={montherror.inerrorstate ? 'error': 'errorfree'} >{montherror.value}</div>
           </div>
           <div className='miniconatiner'>
-            <label>YEAR</label>
-            <input type='text' onChange={(e)=>{setYears(e.target.value)}} value={years} placeholder='YYYY'/>
-            <div className='error' id="erroryear">{yearerror}</div>
+            <label className={yearerror.inerrorstate ? 'datelabel errorlabel': 'datelabel errorfreelabel'}>YEAR</label>
+            <input type='text' onChange={(e)=>{setYears(e.target.value)}} value={years} placeholder='YYYY' className={yearerror.inerrorstate ? 'dateinput errorinput': 'dateinput errorfreeinput'}/>
+            <div className={yearerror.inerrorstate ? 'error': 'errorfree'} >{yearerror.value}</div>
           </div>
         </div>
         <div className='divider'>
@@ -37,32 +37,58 @@ export const DateInput = ({calculateage}) => {
 }
 
 function ProcessDate(days,months,years,calculateage,setDayError,setMonthError,setYearError){
-if(days=="")
+  
+let haserror=false;
+if(days!="" && !Number.isInteger(parseInt(days, 10)) || parseInt(days)>31 || parseInt(days)<1){
+  haserror=true;
+  setDayError({inerrorstate:true,value:"not a valid day"})
+}
+else if(days=="")
 {
-  setDayError("This field is required")
-}
-if(months==""){
-  setMonthError("This field is required")
-}
-if(years==""){
-  setYearError("This field is required")
-}
-if(!Number.isInteger(parseInt(days, 10)) || parseInt(days)>31 || parseInt(days)<1){
-  setDayError("not a valid day")
-}
-if(!Number.isInteger(parseInt(months, 10)) || parseInt(months)>12 || parseInt(months)<1){
-  setMonthError("not a valid month")
-}
-if(!Number.isInteger(parseInt(years, 10)) || new Date().getFullYear()<=parseInt(years)){
-  setYearError("not a valid date")
-}
-
-if(isValidDate(days,months,years)){
-  calculateage(days,months,years)
+  haserror=true;
+  setDayError({inerrorstate:true,value:"This field is required"})
 }
 else{
-  setDayError("not a valid date")
+  
+  setDayError({inerrorstate:false,value:"for days"})
 }
+
+
+if(months!="" && !Number.isInteger(parseInt(months, 10)) || parseInt(months)>12 || parseInt(months)<1){
+  haserror=true;
+  setMonthError({inerrorstate:true,value:"not a valid month"})
+}
+else if(months==""){
+  haserror=true;
+  setMonthError({inerrorstate:true,value:"This field is required"})
+}
+else{
+  setMonthError({inerrorstate:false,value:"for months"})
+}
+
+if(years!="" && !Number.isInteger(parseInt(years, 10)) || new Date().getFullYear()<=parseInt(years)){
+  haserror=true;
+  setYearError({inerrorstate:true,value:"not a valid Year"})
+}
+else if(years==""){
+  haserror=true;
+  setYearError({inerrorstate:true,value:"This field is required"})
+}
+else{
+  setYearError({inerrorstate:false,value:"for years"})
+}
+if(!haserror){
+  if(isValidDate(days,months,years)){
+    calculateage(days,months,years)
+  }
+  else{
+    setDayError({inerrorstate:true,value:"not a valid date"})
+    setMonthError({inerrorstate:true,value:"_"})
+    setYearError({inerrorstate:true ,value:"_"})
+  }
+}
+
+
 
 }
 
@@ -78,7 +104,7 @@ function isValidDate(day, month, year) {
   year = parseInt(year);
 
   // Check if the year is a valid 4-digit number
-  if (year < 1000 || year > 9999) {
+  if (year < 100 || year > 9999) {
     return false;
   }
 
