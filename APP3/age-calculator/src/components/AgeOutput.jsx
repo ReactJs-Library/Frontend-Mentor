292c48d5bda0dc4e127a5f5e68b1bb5f7f447c59
VISH01AL <g.vish1al@gmail.com>
2024-01-28 18:00:21 -0500

    almost done with app3

--- APP3/age-calculator/src/components/AgeOutput.jsx
+++ APP3/age-calculator/src/components/AgeOutput.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import './AgeOutput.css'
 export const AgeOutput = ({years,months,days}) => {
     
   return (
-    <div>
-        <div>{years}years</div>
-        <div>{months}months</div>
-        <div>{days}days</div>
+    <div className='MainContainer'>
+        <div ><span className='ageanswer'>{years}</span> <span className='agetext'>years</span></div>
+        <div><span className='ageanswer'>{months}</span> <span className='agetext'>months</span></div>
+        <div><span className='ageanswer'>{days}</span> <span className='agetext'>days</span></div>
     </div>
   )
 }
