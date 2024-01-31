8800f366a3b03ec325d1669615edc6e02fecdea6
VISH01AL <g.vish1al@gmail.com>
2024-01-31 18:02:46 -0500

    added the header

--- APP5/notifications-page-main/src/Components/Header.jsx
+++ APP5/notifications-page-main/src/Components/Header.jsx
@@ -4,9 +4,12 @@ import styles from './Header.module.css'
 export const Header = ({notificationcount}) => {
   return (
     <div className={styles.container}>
-        <div>Notification</div>
-        <div>{notificationcount}</div>
-        <div>Mark all as read</div>
+        <div className={styles.left}>
+            <div className={styles.Heading}>Notifications</div>
+            <div className={styles.notification}>{notificationcount}</div>
+        </div>
+        
+        <div className={styles.right}>Mark all as read</div>
     </div>
   )
 }
