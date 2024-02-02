af7b4c18e347b0c37f822e6c228ce195f90e7c15
VISH01AL <g.vish1al@gmail.com>
2024-02-02 12:13:33 -0500

    need to add styles added the notifications

--- APP5/notifications-page-main/src/Components/EachNotification.jsx
+++ APP5/notifications-page-main/src/Components/EachNotification.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
-import img from '../images/avatar-angela-gray.webp'
+import styles from './EachNotification.module.css'
+
 export const EachNotification = ({notification}) => {
    
   return (
-    <div>
-        <img src={"/src/"+notification.profileimage} alt="img not found "></img>
-        {notification.name}
+    <div className={notification.read ?  `${styles.container}` : `${styles.container} ${styles.addcolor}`}>
+        <div>
+            <img src={"/src/"+notification.profileimage} alt="img not found "></img>
+            <div>{notification.name}</div>
+            <div>{notification.conent}</div>
+            {notification.messageOrLinkOrImage.haslink ? <div>{notification.link}</div> : <></>}
+            <div>{notification.time}</div>
+            {notification.messageOrLinkOrImage.hasmessage ? <div>{notification.message}</div> : <></>}
+        </div>
+        {notification.messageOrLinkOrImage.hasimage ? <img src={"/src/"+notification.image} alt="img not found "></img> : <></>}
     </div>
   )
 }
