9a04470b17cc6d8128ea7d857493c8812a2a9039
VISH01AL <g.vish1al@gmail.com>
2024-02-06 20:02:50 -0500

    started working on form part

--- APP6/interactive-card-details-form/src/components/Right.jsx
+++ APP6/interactive-card-details-form/src/components/Right.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Right.module.css'
 export const Right = () => {
-
+  const [cardnameerror,setCardNameError]=useState({error:true,message:"no error"})
+  const [cardnumbererror,setCardNumberError]=useState({error:true,message:"no error"})
+  const [expdateerror,setExpDateError]=useState({error:true,message:"no error"})
+  const [mm_yyerror,setMMYYError]=useState({error:true,message:"no error"})
+  const [cvcerror,setCvcError]=useState({error:true,message:"no error"})
   return (
     <div className={styles.container}>
       <div className={styles.minicontainer}>
           <div className={styles.cardholdercontainer}>
               <label className={styles.cardholderlabel}>CARDHOLDER NAME</label>
               <input type='text' max={32} className={styles.cardholderinput}></input>
-              <div className={styles.cardholdererror}>asdf </div>
+              <div className={cardnameerror.error ? styles.cardholdererror : styles.cardholdernoerror}>{cardnameerror.message} </div>
           </div>
           <div className={styles.cardnumbercontainer}>
               <label className={styles.cardnumberlabel}>CARD NUMBER</label>
               <input type='text' max={32} className={styles.cardnumberinput}></input>
-              <div className={styles.cardnumberererror}>asdf </div>
+              <div className={cardnumbererror.error ? styles.cardnumbererror : styles.cardnumbernoerror}>{cardnumbererror.message} </div>
           </div>
-          <label></label>
-          <input type='text' ></input>
-          <div>
-              <label>EXP.DATE</label>
-              <input type='text'></input>
-              <label>(MM/YY)</label>
-              <input type='text'></input>
-              <label>cvc</label>
-              <input type='text'></input>
+          
+          <div className={styles.singlebandinput}>
+              <div className={styles.singlebandinput1}>
+                  <label>EXP.DATE</label>
+                  <input type='text' className={styles.expdateinput}></input>
+                  <div className={expdateerror.error ? styles.expdateerror : styles.expdatenoerror}>{expdateerror.message}</div>
+              </div>
+              <div className={styles.singlebandinput2}>
+                  <label>(MM/YY)</label>
+                  <input type='text' className={styles.mm_yy}></input>
+                  <div className={mm_yyerror.error ? styles.mm_yyerror : styles.mm_yynoerror}>{mm_yyerror.message}</div>
+              </div>
+              <div className={styles.singlebandinput3}>
+                  <label>cvc</label>
+                  <input type='text' className={styles.cvc}></input>
+                  <div className={cvcerror.error ? styles.cvcerror : styles.cvcnoerror}>{cvcerror.message}</div>
+              </div>
           </div>
+
           <button >Confirm</button>
       </div>
         
